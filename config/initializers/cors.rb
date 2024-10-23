c0156104144729a137919eb8aafa8009561fda15
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-10-23 04:51:20 -0300

    fix(app): Adding alias address to cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins "http://127.0.0.1:8080"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
