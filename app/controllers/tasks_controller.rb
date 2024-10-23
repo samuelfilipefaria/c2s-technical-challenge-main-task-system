375c6fd809d1b2085fe5e499b5f16d4a189d4e36
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-10-23 06:22:14 -0300

    feat(app): Adding methods to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ActionController::API
+  before_action :authorize_user, except: :api_message
+
+  def send_response(message, code)
+    render json: {APIresponse: message}, status: code
+  end
+
   def api_message
-    render json: {message: "Olá! Este é o microserviço para TAREFAS"}
+    send_response("Hello! This is the microservice for TASKS", 200)
+  end
+
+  def is_given_token_valid(given_token)
+    authentication_microservice_response = HTTParty.get("http://authentication_microservice_api:5000/users/valid_token?token=#{given_token}")
+    authentication_microservice_response.code == 200
+  end
+
+  def authorize_user
+    send_response("Token is invalid! User not found!", 404) unless is_given_token_valid(params[:token])
   end
 
   def get_all_user_tasks
@@ -12,25 +27,25 @@ class TasksController < ActionController::API
     task = Task.find(params[:id])
 
     if task
-      render json: tag
+      render json: task
     else
-      render json: {error: "Tarefa não encontrada!"}
+      send_response("Task not found!", 404)
     end
   end
 
   def create
     new_task = Task.new(
       description: params[:description],
-      user_id: params[:user_id],
+      user_id: params[:token],
       state: params[:state],
       task_type: params[:task_type],
       url_for_scraping: params[:url_for_scraping],
     )
 
     if new_task.save
-      render json: new_task
+      send_response("Task created!", 201)
     else
-      render json: {error: "Erro ao criar tarefa!"}
+      send_response("Error creating task!", 500)
     end
   end
 
@@ -38,27 +53,29 @@ class TasksController < ActionController::API
     task = Task.find(params[:id])
 
     unless task
-      render json: {error: "Tarefa não encontrada!"}
+      send_response("Task not found!", 404)
       return
     end
 
     task.update(
       description: params[:description],
-      user_id: params[:user_id],
+      task_type: params[:type],
       state: params[:state],
-      type: params[:type],
       url_for_scraping: params[:url_for_scraping],
     )
+
+    send_response("Task updated!", 200)
   end
 
   def delete
-    task = Task.find(params[:task_id])
+    task = Task.find(params[:id])
 
     unless task
-      render json: {error: "Tarefa não encontrada!"}
+      send_response("Task not found!", 404)
       return
     end
 
     task.destroy
+    send_response("Task deleted!", 200)
   end
 end
